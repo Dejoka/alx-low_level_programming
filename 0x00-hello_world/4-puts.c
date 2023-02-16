b73fd9ab9cab70096d17ba83a79aaf98172efded
Dejoka <joshuathankgod227@gmail.com>
2023-02-16 04:51:17 -0800

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that prints exactly a statement follow by a new line
+ * Return: Always 0 (Success)
+ */
+init main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
