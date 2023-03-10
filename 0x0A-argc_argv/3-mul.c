3e3593481e350fb2f660295e1f11e21b735ad970
Dejoka <joshuathankgod227@gmail.com>
2023-03-10 11:05:58 -0800

    program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,33 +2,26 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
+* _atoi - converts a string to an integer
+* @s: string to be converted
+*
+* Return: the int converted from the string
+*/
+
 int _atoi(char *s)
 
 {
 
 	int i, d, n, len, f, digit;
 
-
-
 	i = 0;
-
 	d = 0;
-
 	n = 0;
-
 	len = 0;
-
 	f = 0;
-
 	digit = 0;
 
 	while (s[len] != '\0')
-
 		len++;
 
 	while (i < len && f == 0)
@@ -47,39 +40,41 @@ int _atoi(char *s)
 
 			if (d % 2)
 
-				digit = -digit;
+			digit = -digit;
 
-	n = n * 10 + digit;
+			n = n * 10 + digit;
 
-	f = 1;
+			f = 1;
 
-	if (s[i + 1] < '0' || s[i + 1] > '9')
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 
-		break;
+				break;
 
-	f = 0;
+			f = 0;
 
-	}
+		}
 
-	i++;
+		i++;
 
 	}
 
 	if (f == 0)
 
-	return (0);
+		return (0);
 
 	return (n);
+
 }
 
 /**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
- */
+* main - multiplies two numbers
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: 0 (Success), 1 (Error)
+*/
 int main(int argc, char *argv[])
+
 {
 
 	int result, num1, num2;
@@ -91,7 +86,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 
 		return (1);
-
 	}
 
 	num1 = _atoi(argv[1]);
@@ -100,7 +94,9 @@ int main(int argc, char *argv[])
 
 	result = num1 * num2;
 
+
 	printf("%d\n", result);
+
 
 	return (0);
 }
