439633650d566a0321e74113a67dd67ffead92fc
Dejoka <joshuathankgod227@gmail.com>
2023-03-30 05:55:58 -0700

    0. Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define LISTS_H
 
 /**
- * @struct list_s - singly linked list
+ * @struct: list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: lenght of the string
  * @next: points to the next node
@@ -15,7 +15,8 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+}
+list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
