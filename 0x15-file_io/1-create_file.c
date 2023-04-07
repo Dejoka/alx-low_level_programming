1775e01bc08213491b6818d802bd5b33043e992d
Dejoka <joshuathankgod227@gmail.com>
2023-04-07 13:08:50 -0700

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,9 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		len = strlen(text_content);
+
+		for (len = 0; text_content[len];)
+			len++;
 
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
